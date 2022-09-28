fedb9e96f356acdb6cfef13a1f734f6961a37d2c
igor.vieira4 <igor.p.r.vieira@gmail.com>
2022-09-27 23:48:20 -0300

    add the ideia to create a function, and declare params

---
+++ functions/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+  another_function();
+  another_function_int(5);
+  print_labeled_measurement(5, 'h');
+}
+
+fn another_function() {
+  println!("Another function");
+}
+
+fn another_function_int(x: i32) {
+  println!("The value of x is: {x}")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+  println!("The measurement is: {value}{unit_label}")
+}
